8a4fecb28db6f027379685cce4e42fce1e0967c5
Michael Peacock <michaellynnpeacock@gmail.com>
2026-07-12 00:03:02 -0400

    chore: remove default branding, update metadata for link previews, and strip unused dependencies

--- src/lib/stripe.ts
+++
@@ -1,9 +0,0 @@
-import Stripe from 'stripe';
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2026-06-24.dahlia',
-  appInfo: {
-    name: 'Mimic',
-    version: '0.1.0',
-  },
-});
